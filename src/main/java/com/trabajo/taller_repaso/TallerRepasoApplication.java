c23b0cac281dd65600db4157094d134b254905dc
JaimeBarreraS <Jaenba10@gmail.com>
2025-04-23 10:30:30 -0500

    first commit

---
+++ src/main/java/com/trabajo/taller_repaso/TallerRepasoApplication.java
@@ -0,0 +1,13 @@
+package com.trabajo.taller_repaso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TallerRepasoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TallerRepasoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/trabajo/taller_repaso/TallerRepasoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trabajo.taller_repaso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TallerRepasoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
